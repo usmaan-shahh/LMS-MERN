ed80349ea9a8b74893ddb0305cedad88010ac107
usmaan-shahh <usi.shah06@gmail.com>
2025-04-16 14:36:25 +0100

    fix: clean up comments in authSlice for clarity and remove redundant information

--- client/src/slices/authSlice.js
+++ client/src/slices/authSlice.js
@@ -13,6 +13,12 @@ const authSlice = createSlice({
       state.isAuthenticated = true;
       state.user = action.payload.user;
     },
+
+    // Redux Toolkit automatically creates action creators for the reducers you define. The action creator for userLoggedIn will look like this:
+    // function userLoggedIn(user) {
+    //   return { type: "auth/userLoggedIn", payload: { user } };
+    // }
+
     userLoggedOut: (state) => {
       state.isAuthenticated = false;
       state.user = null;
@@ -22,12 +28,3 @@ const authSlice = createSlice({
 
 export const { userLoggedIn, userLoggedOut } = authSlice.actions;
 export default authSlice.reducer;
-
-// The createSlice function automatically generates action creators and action types based on the slice name and the reducer functions you define. This means you don't have to manually create action types and action creators, which can save you a lot of boilerplate code.
-
-// Redux Toolkit automatically creates action creators for the reducers you define. The action creator for increment will look like this:
-// function increment() {
-//   return { type: "counter/increment",  payload: optionalData };
-// }
-
-// Action Object is a plain object that describes what happened	{ type: 'counter/increment' }

3fb9dab51018210d672834098fc1b4c0df961e71
usmaan-shahh <usi.shah06@gmail.com>
2025-04-16 18:34:31 +0100

    Refactor code structure for improved readability and maintainability

--- client/src/apiSlice/authApi.js
+++ client/src/apiSlice/authApi.js
@@ -2,11 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn } from "../slices/authSlice";
 
 const authApi = createApi({
+  
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:8080/api/v1/user/",
     credentials: "include",
   }),
+
   endpoints: (builder) => ({
     loginUser: builder.mutation({
       query: (inputData) => ({
@@ -15,6 +17,7 @@ const authApi = createApi({
         body: inputData,
       }),
     }),
+
     registerUser: builder.mutation({
       query: (inputData) => ({
         url: "register",

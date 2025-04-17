545b8e30e47a6cb3bbe255d1ff8cb20935eeadff
usmaan-shahh <usi.shah06@gmail.com>
2025-04-17 07:07:36 +0100

    refactor(authSlice): update comments and action type naming
    Improve code readability by updating comments to better explain the Redux Toolkit behavior. Also, correct the action type naming in the comment to match the actual slice name.

--- client/src/slices/authSlice.js
+++ client/src/slices/authSlice.js
@@ -6,10 +6,15 @@ const initialState = {
 };
 
 const authSlice = createSlice({
-  
+
   name: "authSlice",
   initialState,
   reducers: {
+    
+    // Hey Redux! I want to log in this user: { id: 1, name: 'Alice' }"
+    // Redux Toolkit’s userLoggedIn action creator builds an action object:
+    // Redux sends this action object to the reducer
+
     userLoggedIn: (state, action) => {
       state.isAuthenticated = true;
       state.user = action.payload.user;
@@ -17,7 +22,7 @@ const authSlice = createSlice({
 
     // Redux Toolkit automatically creates action creators for the reducers you define. The action creator for userLoggedIn will look like this:
     // function userLoggedIn(user) {
-    //   return { type: "auth/userLoggedIn", payload: { user } };
+    //   return { type: "authSlice/userLoggedIn", payload: { user } };
     // }
 
     userLoggedOut: (state) => {

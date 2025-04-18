b81ceb0e1c4a4fffe9bab4cdf9083ae32f0b7c92
usmaan-shahh <usi.shah06@gmail.com>
2025-04-18 09:53:48 +0100

    fix(Navbar): update user state to reflect logged-out status

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
-  const user = true;
+  const user = false;
   return (
     <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
       {/* Desktop */}

3a47783452d61cd93c51e50f46fc7e1b84499cef
usmaan-shahh <usi.shah06@gmail.com>
2025-04-18 15:54:58 +0100

    fix(Router): add missing newline for improved code readability

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -8,6 +8,7 @@ import {
 import MainLayout from "./layout/MainLayout";
 import Login from "./pages/Login";
 import HeroSection from "./pages/student/HeroSection";
+
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>

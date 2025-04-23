1e9699bc9989154c866a569b92cadaa5be94c9a9
usmaan-shahh <usi.shah06@gmail.com>
2025-04-23 14:01:38 +0100

    refactor(isAuthenticated): improve token handling and enhance code clarity

--- server/middleware/isAutheticated.js
+++ server/middleware/isAutheticated.js
@@ -4,15 +4,15 @@ const isAuthenticated = (req, res, next) => {
   const token = req.cookies?.Token;
   if (!token) {
     return res
-      .status(401)
+      .status(401) //401 Unauthorized – Authentication required or token invalid/missing.
       .json({ message: "No token provided", success: false });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.id = decoded.id;
-
+    console.log(decoded);
+    const { id } = decoded;
+    req.id = id; // Attach the user ID to the request object for further use in the application.
     next();
   } catch (error) {
     console.error("JWT verification error:", error.message);

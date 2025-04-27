020bcae0a20b5152b513db706ec838d5d38bba75
usmaan-shahh <usi.shah06@gmail.com>
2025-04-27 07:36:55 +0100

    Add comments to clarify the purpose of upload and delete functions in Cloudinary configuration

--- server/configuration/cloudinary.js
+++ server/configuration/cloudinary.js
@@ -7,7 +7,7 @@ cloudinary.config({
   api_secret: process.env.API_SECRET,
   cloud_name: process.env.CLOUD_NAME,
 });
-
+// uploadMedia is an async function that uploads a file to Cloudinary and returns the upload response from Cloudinary, which contains details about the uploaded file
 export const uploadMedia = async (file) => {
   try {
     const uploadResponse = await cloudinary.uploader.upload(file, {
@@ -18,7 +18,7 @@ export const uploadMedia = async (file) => {
     console.log(error);
   }
 };
-
+// By default, cloudinary.uploader.destroy(publicId) deletes resources of type "image" or "raw" (generic files), unless you specify a different resource_type.
 export const deleteMediaFromCloudinary = async (publicId) => {
   try {
     await cloudinary.uploader.destroy(publicId);
@@ -26,7 +26,7 @@ export const deleteMediaFromCloudinary = async (publicId) => {
     console.log(error);
   }
 };
-
+// deleteVideoFromCloudinary is an async function that deletes a video from Cloudinary using its public ID
 export const deleteVideoFromCloudinary = async (publicId) => {
   try {
     await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
